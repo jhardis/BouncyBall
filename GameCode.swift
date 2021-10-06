1fc4405ebf4869821db94d0977da55b4e581cbe0
Jonathan E. Hardis <jhardis@me.com>
2021-10-05 20:18:59 -0400

    End of Part 5

--- GameCode.swift
+++ GameCode.swift
@@ -24,7 +24,14 @@ let funnelPoints = [                                    // p. 227
                                                         // p. 227
 let funnel = PolygonShape(points: funnelPoints)         // p. 227
 
-
+let targetPoints = [                                    // p. 236
+    Point(x: 10, y: 0),                                 // p. 236
+    Point(x: 0, y: 10),                                 // p. 236
+    Point(x: 10, y: 20),                                // p. 236
+    Point(x: 20, y: 10)                                 // p. 236
+]                                                       // p. 236
+                                                        // p. 236
+let target = PolygonShape(points: targetPoints)         // p. 236
 
 
 
@@ -39,10 +46,11 @@ of a function.
 */
 
 fileprivate func setupBall() {
-    ball.position = Point(x: 250, y: 400)             // p. 223
-    ball.hasPhysics = true                            // p. 224
+    ball.position = Point(x: 250, y: 400)               // p. 223
+    ball.hasPhysics = true                              // p. 224
     ball.fillColor = .blue
-    scene.add(ball)                                   // p. 223
+    ball.onCollision = ballCollided(with:)              // p. 240
+    scene.add(ball)                                     // p. 223
 }
 
 fileprivate func setupBarrier() {
@@ -62,18 +70,34 @@ fileprivate func setupFunnel() {
     scene.add(funnel)                                   // p. 228
 }
 
+fileprivate func setupTarget() {                        // p. 237
+    // Add a target to the scene                        // p. 237
+    target.position = Point(x: 200, y: 400)             // p. 237
+    target.hasPhysics = true                            // p. 237
+    target.isImmobile = true                            // p. 237
+    target.isImpermeable = true                         // p. 237
+    target.fillColor = .yellow                          // p. 237
+    target.name = "target"                              // p. 241
+    scene.add(target)                                   // p. 237
+}
+
 func setup() {
     setupBall()                                         // p. 234
     setupBarrier()                                      // p. 234
     setupFunnel()                                       // p. 234
+    setupTarget()                                       // p. 237
     
 }
 
 // Drops the ball by moving it to the funnel's position // p. 229
 func dropBall() {                                       // p. 229
-    ball.position = funnel.position                   // p. 229
+    ball.position = funnel.position                     // p. 229
 }
 
-
+// Handles collisions between the ball and the targets  // p. 240
+func ballCollided(with otherShape: Shape) {             // p. 240
+    if otherShape.name != "target" {return}             // p. 241
+    otherShape.fillColor = .green                       // p. 240
+}                                                       // p. 240
 
 
